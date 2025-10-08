12c0143ada479f715f56fa4d495e70ef9c86a316
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-10-08 14:15:29 +0100

    Trustgraph basic websocket client

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// @trustgraph/client
+// TrustGraph TypeScript Client
+
+// Export models (data types)
+export * from "./models/Triple";
+export * from "./models/messages";
+
+// Export socket client
+export * from "./socket/trustgraph-socket";
